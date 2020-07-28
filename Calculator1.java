70e3ff3d7396f34df71e391780c3eca0e20374fa
Jainam Swamy <jainamswamy2005@gmail.com>
2020-07-28 16:57:48 +0530

    .

---
+++ Calculator1.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+class Calculator1
+{
+    static double x,y;
+    static char ch;
+    Calculator1()
+    {
+        Scanner sc=new Scanner(System.in);
+        double a=0;
+        System.out.println("Enter the Internet usage(in a):");
+        a=sc.nextInt();
+        x=a;
+        Scanner sb=new Scanner(System.in);
+        double b=0;
+        System.out.println("Enter the Internet usage(in b):");
+        b=sb.nextInt();
+        y=b;
+        Scanner sa=new Scanner(System.in);
+        char c='\u0000';
+        System.out.println("Enter the Internet usage(in c):");
+        c=sa.next().charAt(0);
+        ch=c;
+    }
+    double Addition()
+    {
+        double add=x+y;
+        return add;
+    }
+    double Subtraction()
+    {
+        double subtract=x-y;
+        return subtract;
+    }
+    double Multiplication()
+    {
+        double multiply=x*y;
+        return multiply;
+    }
+    double Division()
+    {
+        double quotient=x/y;
+        return quotient;
+    }
+    public static void main()
+    {
+        Calculator1 obj=new Calculator1();
+        switch(ch)
+        {
+            case '+':
+            System.out.println("The sum is"+ obj.Addition());
+            break;
+            case '-':
+            System.out.println("The subtracted amount is"+ obj.Subtraction());
+            break;
+            case '*':
+            System.out.println("The product is"+ obj.Multiplication());
+            break;
+            case '/':
+            System.out.println("The quotient is"+ obj.Division());
+            break;
+            default:
+            System.out.println("Wrong Character");
+            break;
+        }
+    }
+}
