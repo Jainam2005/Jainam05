4524f51ff8093c1fa6dc6e7eaa67f1e095916ff3
Jainam Swamy <jainamswamy2005@gmail.com>
2020-08-04 10:11:17 +0530

    .

---
+++ BigLife.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class BigLife
+{
+    double Discount, NetAmount, PurchasedAmount;
+    void input()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the purchased amount");
+        PurchasedAmount=sc.nextDouble();
+    }
+    void compute()
+    {
+        if(PurchasedAmount<=5000)
+        {
+            Discount=400;
+            NetAmount=PurchasedAmount-Discount;
+        }
+        else if(PurchasedAmount>5000 && PurchasedAmount<=10000)
+        {
+            Discount=800;
+            NetAmount=PurchasedAmount-Discount;
+        }
+        else if(PurchasedAmount>10000 && PurchasedAmount<=20000)
+        {
+            Discount=1000;
+            NetAmount=PurchasedAmount-Discount;
+        }
+        else
+        {
+            Discount=(PurchasedAmount-2000)*3/100;
+            NetAmount=PurchasedAmount-2000-Discount;
+        }
+    }
+    void display()
+    {
+        System.out.println("The Net Amount is"+NetAmount);
+        System.out.println("The discount is"+Discount);
+        System.out.println("The purchased amount is"+PurchasedAmount);
+    }
+    public static void main()
+    {
+        BigLife obj=new BigLife();
+        obj.input();
+        obj.compute();
+        obj.display();
+    }
+}
---
+++ Greater.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Greater
+{
+    double num1, num2, num3, greatest;
+    void input()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number");
+        num1=sc.nextDouble();
+        Scanner sb=new Scanner(System.in);
+        System.out.println("Enter the second number");
+        num2=sb.nextDouble();
+        Scanner sa=new Scanner(System.in);
+        System.out.println("Enter the third number");
+        num3=sa.nextDouble();
+    }
+    void compute()
+    {
+        if(num1>num2 && num1>num3 && num1>0)
+        {
+            greatest=num1;
+            System.out.println("The greatest number is"+greatest);
+        }
+        else if(num2>num1 && num2>num3 && num2>0)
+        {
+            greatest=num2;
+            System.out.println("The greatest number is"+greatest);
+        }
+        else if(num3>num1 && num3 >num2 && num3>0)
+        {
+            greatest=num3;
+            System.out.println("The greatest number is"+greatest);
+        }
+        else
+        {
+            System.out.println("Zero value is not allowed");
+        }
+    }
+    public static void main()
+    {
+        Greater obj=new Greater();
+        obj.input();
+        obj.compute();
+    }
+}
