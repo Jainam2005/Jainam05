08fb0c3751ea70820437b97a2ffa79588ee8ccfa
Jainam Swamy <jainamswamy2005@gmail.com>
2020-11-13 16:33:30 +0530

    .

--- BoardQuestion9.java
+++ BoardQuestion9.java
@@ -1,29 +1,42 @@
+/*9. Write a program to calculate and print the sum of each of the following series:
+(a) Sum = 2-4+6-8+10……-20
+(b) Sum = x/2 + x/5 + x/8 + x/11 + …..+ x/20   (x to be input by user)*/
+import java.util.Scanner;
 public class BoardQuestion9
 {
-    void series1()
+    void seriesA()
     {
-        double s=0;
-        for(double i=2.0;i<21;i+=2)
+        double sum=0;
+        for(int i=2;i<21;i+=2)
         {
-            double n=i;
+            int n=i;
             if(n%4!=0)
             {
-                s=s+n;
+                sum=sum+n;
             }
             else
             {
-                s=s-n;
+                sum=sum-n;
             }
         }
-        System.out.println(s);
+        System.out.println("The sum of the first series is"+sum);
     }
-    void series2(double x)
+    void seriesB()
     {
-        double sum=0;
-        for(double i=2;i<=20;i+=3)
+        double x, sum=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of x");
+        x=sc.nextDouble();
+        for(int i=2;i<=20;i+=3)
         {
             sum=sum+(x/i);
         }
-        System.out.println(sum);
-    } 
+        System.out.println("The sum of the second series is"+sum);
+    }
+    public static void main()
+    {
+        BoardQuestion9 obj=new BoardQuestion9();
+        obj.seriesA();
+        obj.seriesB();
+    }
 }
