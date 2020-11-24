ea048ddaa06e36973a557459e3e33decc26bf21f
Jainam Swamy <jainamswamy2005@gmail.com>
2020-11-24 12:02:56 +0530

    .

---
+++ BoardQuestion12.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class BoardQuestion12
+{
+    char[] nArray;
+    BoardQuestion12()
+    {
+        nArray=new char[20];
+    }
+    void initialize()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the 20 Characters");
+        for(int i=0;i<=nArray.length-1;i++)
+        {
+            nArray[i]=sc.next().charAt(0);
+        }
+    }
+    void display()
+    {
+        System.out.println("Sorted Array:");
+        for(int i=0;i<=nArray.length-1;i++)
+        {
+            System.out.print(""+nArray[i]+",");
+        }
+        System.out.println();
+    }
+    void arrange()
+    {
+        char temp;
+        for(int i=0;i<=nArray.length-2;i++)
+        {
+            for(int j=0;j<=nArray.length-2;j++)
+            {
+                if(nArray[j]<nArray[j+1])
+                {
+                    temp=nArray[j];
+                    nArray[j]=nArray[j+1];
+                    nArray[j+1]=temp;
+                }
+            }
+        }
+        display();
+    }
+    public static void main()
+    {
+        BoardQuestion12 obj=new BoardQuestion12();
+        obj.initialize();
+        obj.arrange();
+    }
+}
