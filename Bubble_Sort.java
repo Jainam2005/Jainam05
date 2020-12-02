9d9e15d667a5ba5577914a25f1a203a1e4c7b742
Jainam Swamy <jainamswamy2005@gmail.com>
2020-12-03 00:19:40 +0530

    .

---
+++ Bubble_Sort.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Bubble_Sort
+{
+    int[] nArray;
+    Bubble_Sort(int arrSize)
+    {
+        nArray=new int[arrSize];
+    }
+    void initialize()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Elements of the Array");
+        for(int i=0;i<=nArray.length-1;i++)
+        {
+            nArray[i]=sc.nextInt();
+        }
+    }
+    void display()
+    {
+        for(int i=0;i<=nArray.length-1;i++)
+        {
+            System.out.print(" "+nArray[i]+" ");
+        }
+        System.out.println();
+    }
+    void arrange()
+    {
+        int temp;
+        for(int i=0;i<=nArray.length-2;i++)
+        {
+            for(int j=0;j<=nArray.length-2;j++)
+            {
+                if(nArray[j]>nArray[j+1])
+                {
+                    temp=nArray[j];
+                    nArray[j]=nArray[j+1];
+                    nArray[j+1]=temp;
+                }
+            }
+            System.out.print("Pass/Itertation: "+(i+1)+" ==> ");
+            display();
+        }
+    }
+    public static void main()
+    {
+        Bubble_Sort obj=new Bubble_Sort(5);
+        obj.initialize();
+        obj.arrange();
+    }
+}
